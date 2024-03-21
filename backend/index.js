294e5e51d1bba9604a41f9f48e2eb26db4403b96
ashishverma94 <hariashish1@gmail.com>
2024-03-21 19:34:48 +0530

    commit 8

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,17 @@
 const cors = require("cors");
 const express = require("express");
-// const connectDatabase = require("./db/Database.js");
+const connectDatabase = require("./db/Database.js");
  
-// require("dotenv").config();
+require("dotenv").config();
 
 const app = express();
 
-// app.use(
-//   cors({
-//     origin: ["http://localhost:5173"],
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
  
 app.get("/test", (req, res) => {
@@ -19,10 +19,10 @@ app.get("/test", (req, res) => {
 });
 
 // Routes
-// const user = require("./controller/codeController.js");
-// app.use("/api/v1/code", user);
+const user = require("./controller/codeController.js");
+app.use("/api/v1/code", user);
 
-// connectDatabase();
+connectDatabase();
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running on port ${process.env.PORT || 3000}`);
 });
